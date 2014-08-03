141df2011d514e574e61f46411a4e05faab66a55
Stephen Nguyen <nguyenhstephen@gmail.com>
2014-08-03 13:31:22 -0700

    cleanup

--- test.rb
+++ test.rb
@@ -1,48 +1,19 @@
 # hey guys this is line 1
 
 def hello
-  p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-
 end
 
 class Git
 
   def initialize(name)
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
-    p "hello"
   	@name = name
-    p "hello"
   end
 
 end
 
-# this is Evan's comment
 def my_method
   puts "Evan"
+end
+
+class Tree
 end
