ab8a22a64bcfc7355bcc77a498f6a0af4d698fdd
Stephen Nguyen <nguyenhstephen@gmail.com>
2014-08-03 12:42:16 -0700

    my crazy changes

--- test.rb
+++ test.rb
@@ -2,13 +2,43 @@
 
 def hello
   p "hello"
-end
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
 
+end
 
 class Git
 
   def initialize(name)
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
+    p "hello"
   	@name = name
+    p "hello"
   end
 
 end
+# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1# hey guys this is line 1s
